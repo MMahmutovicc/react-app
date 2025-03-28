41aa6596e5216f1c4aac119d0c3bf41c2267b44f
Mirza <mmahmutovi3@etf.unsa.ba>
2025-03-28 12:52:54 +0100

    modified api request

--- src/api.js
+++ src/api.js
@@ -4,9 +4,7 @@ const BASE_URL =  process.env.REACT_APP_API_URL;
 
 export const loginUser = async (username, password) => {
     try {
-        const response = await axios.post(`${BASE_URL}/login`, { username, password }, {
-            withCredentials: true
-        });
+        const response = await axios.post(`${BASE_URL}/login`, { username, password });
         return response.data;
     } catch (error) {
         console.error("Login error:", error);
